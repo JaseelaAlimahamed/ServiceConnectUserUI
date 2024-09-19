af24aaa787c54c5f3ea4ba13e3196ae6e200748e
Abhinav-mad <abhinavmad4@gmail.com>
2024-09-19 19:38:52 +0530

    Updated Banner component

--- src/components/user/homePageComponents/Banner.jsx
+++ src/components/user/homePageComponents/Banner.jsx
@@ -1,31 +1,62 @@
-import React from 'react'
-import Slider from "react-slick";
+import React, { useState } from 'react'
 
+import Slider from 'react-slick';
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
 
-const Banner = ({ discount, title, description, buttonText }) => {
-  
+const Banner = ({banners}) => {
+  const [currentSlide,setCurrentSlide]= useState(0)
+  const settings = {
+    dots: false,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    afterChange: (index) => setCurrentSlide(index)
+  };
+
   return (
-    <div className="relative bg-medium-dark-gray  text-white p-6 md:p-10  rounded-3xl shadow-lg h-96 w-full lg:w-3/5 sm:w-96  mx-auto">
+    <Slider {...settings}>{
+    banners.map((banner,Index)=>(
+
+    
+    <div className="relative bg-medium-dark-gray  text-white p-6 md:p-10  rounded-3xl shadow-lg h-96  lg:w-3/5 sm:w-96  mx-auto">
         {/* Background Shapes */}
-     <img src='./Path 3.svg' className='absolute  top-0 left-0 z-0' />
+     <img src='./Path 3.svg' className='absolute w-80  top-0 left-0 z-0' />
       <img src='./Path 2.svg' className='absolute bottom-0 right-0 z-0'/>
        {/* Text Content */}
-      <div className="relative z-10 flex mt-5  md:flex-row items-center justify-between">
+      <div className="relative z-10 flex mt-5   md:flex-row items-center justify-between">
         <div className="text-center md:text-left mt-2 mb-4 md:mb-0">
-          <h2 className="text-2xl md:text-5xl font-bold">{discount}</h2>
-          <h3 className="text-xl md:text-4xl mt-2">{title}</h3>
-          <p className="text-sm md:text-2xl mt-5">{description}</p>
+          <h2 className="text-2xl md:text-5xl font-bold">{banner.discount}</h2>
+          <h3 className="text-xl md:text-4xl mt-2">{banner.title}</h3>
+          <p className="text-sm md:text-2xl mt-5">{banner.description}</p>
         </div>
         {/* Button (Optional if needed) */}
         <button className=" text-white py-3 px-4 bg-medium-dark-gray rounded hover:bg-pink-600 transition duration-300 z-10">
-        {buttonText}
+        {banner.buttonText}
         </button>
       </div>
+
+
+      <div className="absolute bottom-4 left-0 right-0 flex justify-center">
+              {banners.map((_, index) => (
+                <div
+                  key={index}
+                  className={`w-3 h-3 mx-1 rounded-full ${
+                    index === currentSlide ? 'bg-yellow-500' : 'bg-gray-500'
+                  }`}
+                ></div>
+              ))}
+            </div>
      
      
     </div>
+))}
 
-  )
+
+    </Slider>
+
+)
 }
 
 
