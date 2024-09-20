d1f091802a88ada89b1f063d28f9bee5f1fd8303
Aswin <aswinkrishnan.stackup@gmail.com>
2024-09-20 20:39:15 +0530

    reduced card size

--- src/components/reUsableComponents/ServiceCategoryCard.jsx
+++ src/components/reUsableComponents/ServiceCategoryCard.jsx
@@ -2,8 +2,8 @@ import React, { useState } from "react";
 
 const ServiceCategoryCard = ({ imgUrl, category, }) => {
   return (
-    <div className="w-[128px] h-[128px] text-center">
-      <div className="w-[128px] h-[110px] border-4 border-dark-violet rounded-3xl p-[5px]">
+    <div className="w-[120px] h-[128px] flex flex-col items-center text-center">
+      <div className="w-[97px] h-[87px]  border-[3px] border-dark-violet rounded-3xl p-[2px]">
         <img className="w-full h-full rounded-3xl" src={imgUrl} alt="" />
       </div>
       <h6 className="text-dark-violet font-semibold">{category}</h6>
@@ -12,3 +12,4 @@ const ServiceCategoryCard = ({ imgUrl, category, }) => {
 };
 
 export default ServiceCategoryCard;
+
