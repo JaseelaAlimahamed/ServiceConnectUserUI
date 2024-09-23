38df0ccec7cb36dcf4ebda84ae90d32409f3a8cb
Nikhil Babu <nikhilbabu80942@gmail.com>
2024-09-23 15:16:25 +0530

    modified: rearraged app.js

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,12 @@
 import { Routes, Route } from 'react-router-dom'
 import UserRoutes from './routes/UserRoutes';
-import UserLayout from './layouts/UserLayout';
 // import'./App.css';
 function App() {
 
 
   return (
     <Routes>
-       <Route path="/*" element={<UserLayout />}>
-          <Route path="*" element={<UserRoutes />} />
-        </Route>
+          <Route path="/*" element={<UserRoutes />} />
     </Routes>
   );
 }
