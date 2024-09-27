eb8c890143fb9e3770b4cd0a0e06c10f7707a4df
Nihal Mohamed Bashir <nihalmb23@gmail.com>
2024-09-27 19:31:29 +0530

    error cleared

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,14 @@ import UserRoutes from './routes/UserRoutes';
 // import'./App.css';
 function App() {
 
-
   return (
     <Routes>
+
       <Route path="/*" element={<UserRoutes />} />
+
     </Routes>
   );
 }
+
 
 export default App;
