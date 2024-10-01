e7a1315f0016c9c06bae9882196f2fd945441837
Deepu Das <deepudevadasanbkm@gmail.com>
2024-10-01 12:03:34 +0530

    updating

---
+++ src/pages/paymentMethodsPages/PaymentMethodPage.jsx
@@ -0,0 +1,13 @@
+/* eslint-disable no-unused-vars */
+import React from 'react'
+import PaymentMethodsPageComponent from '../../components/user/paymentMethodsComponents/PaymentMethodsPageComponent'
+
+const PaymentMethodPage = () => {
+  return (
+    <div>
+      <PaymentMethodsPageComponent />
+    </div>
+  )
+}
+
+export default PaymentMethodPage
