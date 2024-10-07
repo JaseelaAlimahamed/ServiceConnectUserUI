a65a6adc80726239bfa40c42e4a7f51c5bb6c7c1
Shreejith-S <shreejiths619@gmail.com>
2024-10-07 18:47:16 +0530

    About Page U38

---
+++ src/pages/aboutPage/AboutPage.jsx
@@ -0,0 +1,41 @@
+import React from 'react';
+
+const AboutPage = () => {
+  return (
+    <div className="bg-white min-h-screen p-6">
+
+      {/* Content Section */}
+      <div className="mt-8">
+        {/* First section */}
+        <div className="mb-6">
+          <h2 className="text-dark-gray text-lg font-bold">dasdasdas</h2>
+          <p className="text-gray-600 mt-2">
+            At enim hic etiam dolore. Dulce amarum, leve asperum, prope longe, stare movere, quadratum rotundum.
+            At certe gravius. Nullus est igitur cuiusquam dies natalis. Paulum, cum regem Persem captum adduceret,
+            eodem flumine invectio?
+          </p>
+          <p className="text-gray-600 mt-4">
+            Quare hoc videndum est, possitne nobis hoc ratio philosophorum dare. Sed finge non solum callidum eum,
+            qui aliquid improbe faciat, verum etiam prae potentem, ut M. Est autem officium, quod ita factum est,
+            ut eius facti probabilis ratio reddi possit.
+          </p>
+        </div>
+
+        {/* Second section */}
+        <div className="mb-6">
+          <h2 className="text-dark-gray text-lg font-bold">sljkhbhad</h2>
+          <p className="text-gray-600 mt-2">
+            Ut proverbia non nulla veriora sint quam vestra dogmata. Tamen aberramus a proposito, et, ne longius,
+            prorsus, inquam, Piso, si ista mala sunt, placet. Omnes enim iucundum motum, quo sensus hilaretur.
+          </p>
+          <p className="text-gray-600 mt-4">
+            Dicam, inquam, et quidem discendi causa magis, quam quo te aut Epicurum reprehensum velim. Dolor ergo,
+            id est summum malum, metuitur semper, etiamsi non ader.
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default AboutPage;
--- src/routes/UserRoutes.jsx
+++ src/routes/UserRoutes.jsx
@@ -1,7 +1,5 @@
-
-
-
 import React from "react";
+import Routes from "react-router-dom/Routes";
 
 import AllCategoryComponent from "../components/user/allCategoryAndSubCategoryComponent/allCategoryComponent/AllCategoryComponent";
 import SubCategoryComponent from "../components/user/allCategoryAndSubCategoryComponent/subcategoryComponent/SubCategoryComponent";
