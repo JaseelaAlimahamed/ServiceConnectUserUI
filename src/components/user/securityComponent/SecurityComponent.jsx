450a37c86de36d1e38f4fd4ae12725e31fa42d52
Jitto Chacko <jittocj15@gmail.com>
2024-10-17 09:21:35 +0530

    feature-u44-security page

--- src/components/user/securityComponent/SecurityComponent.jsx
+++ src/components/user/securityComponent/SecurityComponent.jsx
@@ -7,9 +7,11 @@ const securityOption = [
   { id: 3, label: "Face ID" },
 ];
 const SecurityComponent = () => {
-    const [securityOptions, setSecurityOptions] = useState(
-        Array(securityOption.length).fill(false)
-      );
+      const [securityOptions, setSecurityOptions] = useState(
+        Array(securityOption.length).fill(false));
+      const [pinChange, setPinChange] = useState(false);
+      const [passwordChange, setPasswordChange] = useState(false);
+      const [googleAuthenticator,setGoogleAuthenticator] =useState(false)
       console.log(securityOptions)
       
       const handleToggle = (index) => {
@@ -19,6 +21,23 @@ const SecurityComponent = () => {
           return newState;
         });
       };
+
+      const handleChangePin = () => {
+        setPinChange(true);
+        console.log("PIN changed");
+      };
+    
+    
+      const handleChangePassword = () => {
+        setPasswordChange(true);
+      
+        console.log("Password changed");
+      };
+
+      const handleGoogleAuthenticator =()=>{
+        setGoogleAuthenticator(true);
+        console.log("opened google Authenticator")
+      }
     
       return (
         <div className="bg-primary h-screen w-full px-4 sm:px-6 md:px-8 lg:px-16">
@@ -53,17 +72,23 @@ const SecurityComponent = () => {
             p-4 sm:p-6 mx-auto max-w-screen-sm md:max-w-screen-md lg:max-w-screen-lg
             hover:outline-none focus:outline-none"
           >
-            <h2 className="flex w-full items-center justify-between text-sm sm:text-md md:text-lg lg:text-xl font-medium">
+            <h2 className="flex w-full items-center justify-between text-sm sm:text-md md:text-lg lg:text-xl font-medium"
+            onClick={handleGoogleAuthenticator}
+            >
               Google Authenticator
               <GoChevronRight className="w-6 h-7 sm:w-7 sm:h-7 md:w-8 md:h-8 lg:w-9 lg:h-9" />
             </h2>
           </div>
     
           <div className="flex flex-col gap-6 items-center mt-80 w-full">
-            <button className="w-[90%] sm:w-[80%] md:w-[60%] lg:w-[50%] h-14 text-black rounded-full font-medium bg-primary border border-light-gray shadow-[0px_4px_4px_0px_#00000070] text-sm sm:text-md md:text-lg lg:text-xl">
+            <button className="w-[90%] sm:w-[80%] md:w-[60%] lg:w-[50%] h-14 text-black rounded-full font-medium bg-primary border border-light-gray shadow-[0px_4px_4px_0px_#00000070] text-sm sm:text-md md:text-lg lg:text-xl"
+            onClick={handleChangePin}
+            >
               Change PIN
             </button>
-            <button className="w-[90%] sm:w-[80%] md:w-[60%] lg:w-[50%] h-14 text-white rounded-full font-medium bg-dark-gray shadow-[0px_4px_4px_0px_#00000070] text-sm sm:text-md md:text-lg lg:text-xl">
+            <button className="w-[90%] sm:w-[80%] md:w-[60%] lg:w-[50%] h-14 text-white rounded-full font-medium bg-dark-gray shadow-[0px_4px_4px_0px_#00000070] text-sm sm:text-md md:text-lg lg:text-xl"
+              onClick={handleChangePassword}
+            >
               Change Password
             </button>
           </div>
