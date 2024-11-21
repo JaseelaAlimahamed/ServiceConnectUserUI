04b7a891cc71f7c489c0668035e1549106b7281b
MuhammedFahizk <fahizk100@gmail.com>
2024-11-21 17:35:47 +0530

    Revert "feat:design catogories"
    This reverts commit f0335392e17cea3ccc697e64707baf5e07b5dbaa.

--- src/components/user/allCategoryAndSubCategoryComponent/allCategoryComponent/AllCategoryComponent.jsx
+++ src/components/user/allCategoryAndSubCategoryComponent/allCategoryComponent/AllCategoryComponent.jsx
@@ -52,11 +52,11 @@ const AllCategoryComponent = () => {
         md:gap-8 
         lg:gap-12 
         xl:gap-16 
-        grid-cols-2 
+        grid-cols-3 
         sm:grid-cols-1 
-        md:grid-cols-3
-        lg:grid-cols-3
-        xl:grid-cols-4
+        md:grid-cols-4
+        lg:grid-cols-4
+        xl:grid-cols-6
         2xl:grid-cols-6"
       >
         {displayedCategories.map((category) => (
